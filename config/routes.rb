d8e4e09accb5205bc88fb2d2c0083a41feedafd4
Philip Bedward <philipbed813@gmail.com>
2017-09-21 10:21:12 -0400

    updated coming soon page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   get "/event", to: "home#event"
   get "/apilist", to: "home#apilist"
-  get "home/index"
+  get "home/index", to: "home#comingsoon" ##ask about where this should route to
   get "/design_category", to: "home#design_category"
   root to: 'home#index'
 end
